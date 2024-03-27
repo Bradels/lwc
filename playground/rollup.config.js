3375fbfb859ae274f4851036a8219a264e7f4486
Will Harney <wharney@salesforce.com>
2024-03-27 16:43:37 -0400

    chore: rename files to .ts

--- playground/rollup.config.js
+++ playground/rollup.config.js
@@ -2,19 +2,22 @@ import lwc from '@lwc/rollup-plugin';
 import replace from '@rollup/plugin-replace';
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
+import typescript from '@rollup/plugin-typescript';
 
 const __ENV__ = process.env.NODE_ENV ?? 'development';
 
 export default (args) => {
     return {
-        input: 'src/main.js',
+        input: 'src/main.ts',
 
         output: {
             file: 'dist/main.js',
             format: 'esm',
+            sourcemap: true,
         },
 
         plugins: [
+            typescript(),
             replace({
                 'process.env.NODE_ENV': JSON.stringify(__ENV__),
                 preventAssignment: true,
