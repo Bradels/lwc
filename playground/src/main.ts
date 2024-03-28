2349d5d40052800b5adfb26c815a4efd705df4e7
Will Harney <wharney@salesforce.com>
2024-03-28 11:10:46 -0400

    fix: temp workaround for broken module resolution

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,5 +1,6 @@
-import { createElement } from "lwc";
-import App from "x/app";
+import { createElement } from 'lwc';
+// This is a problem that we'll need to fix...
+import App from './modules/x/app/app';
 
-const elm = createElement("x-app", { is: App });
+const elm = createElement('x-app', { is: App });
 document.body.appendChild(elm);
