2d68c05e9f882e55fc683939108a5152417f9539
Will Harney <wharney@salesforce.com>
2024-03-29 15:36:59 -0400

    feat: poc dynamic paths generation

--- playground/rollup.config.js
+++ playground/rollup.config.js
@@ -1,10 +1,51 @@
+import path from 'node:path';
+import fs from 'node:fs';
 import lwc from '@lwc/rollup-plugin';
 import replace from '@rollup/plugin-replace';
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
 import typescript from '@rollup/plugin-typescript';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const loadJson = (file) => {
+    const text = fs.readFileSync(path.join(__dirname, file), 'utf8');
+    const clean = text.replace(/\s*\/\/.*?\n/g, '');
+    return JSON.parse(clean);
+};
+const tsconfig = loadJson('./tsconfig.json');
+const lwcConfig = loadJson('./lwc.config.json');
 
 const __ENV__ = process.env.NODE_ENV ?? 'development';
+
+function getPathsInDirectory(dirname) {
+    const paths = {};
+    const basePath = path.resolve(__dirname, dirname);
+    for (const ns of fs.readdirSync(basePath, { withFileTypes: true })) {
+        if (!ns.isDirectory()) continue;
+        const nsPath = path.join(basePath, ns.name);
+        for (const cmp of fs.readdirSync(nsPath, { withFileTypes: true })) {
+            if (!cmp.isDirectory()) continue;
+            const cmpPath = path.join(nsPath, cmp.name, cmp.name + '.ts');
+            if (!fs.statSync(cmpPath).isFile()) continue;
+            const alias = `${ns.name}/${cmp.name}`;
+            const resolved = '.' + path.sep + path.relative(__dirname, cmpPath);
+            paths[alias] = [resolved];
+        }
+    }
+    return paths;
+}
+
+function getPathsFromConfig() {
+    const paths = {};
+    for (const mod of lwcConfig.modules) {
+        if (mod.dir) Object.assign(paths, getPathsInDirectory(mod.dir));
+    }
+    console.log(paths);
+    return paths;
+}
 
 export default (args) => {
     return {
@@ -17,7 +58,12 @@ export default (args) => {
         },
 
         plugins: [
-            typescript(),
+            typescript({
+                paths: {
+                    ...getPathsFromConfig(),
+                    ...tsconfig.compilerOptions.paths,
+                },
+            }),
             replace({
                 'process.env.NODE_ENV': JSON.stringify(__ENV__),
                 preventAssignment: true,
--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,6 +1,6 @@
 import { createElement } from 'lwc';
-// This is a problem that we'll need to fix...
-import App from './modules/x/app/app';
+// This currently resolves to /src/modules/x/app/index.ts rather than app.ts
+import App from 'x/app';
 
 const elm = createElement('x-app', { is: App });
 document.body.appendChild(elm);
